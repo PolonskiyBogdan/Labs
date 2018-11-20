bcf75b8696994b102276df59695445ea6aa731af
PolonskiyBogdan <polonskiybogdan@gmail.com>
2018-11-20 18:47:20 +0200

    Lab01 ver-1.1 (update BubEnd)

--- Lab01/src/main/java/output/Main.java
+++ Lab01/src/main/java/output/Main.java
@@ -11,46 +11,46 @@ public class Main {
     public static void main(String[] args) {
         Analyze a = new Analyze();
 
-       a.anArrSrtGR();
-       a.anArrSrtMiMa();
-       a.anArrSrtMiMaRx();
-       a.anArrSrtMaMi();
-
-       a.anBubStaGR();
-       a.anBubStaMiMa();
-       a.anBubStaMiMaRx();
-       a.anBubStaMaMi();
-
-
-        a.anBubEndGR();
-       a.anBubEndMiMa();
-       a.anBubEndMiMaRx();
-       a.anBubEndMaMi();
-
-       a.anQuickGR();
-       a.anQuickMiMa();
-       a.anQuickMiMaRx();
-       a.anQuickMaMi();
-
-       a.anMerArrSrtGR();
-       a.anMerArrSrtMiMa();
-       a.anMerArrSrtMiMaRx();
-       a.anMerArrSrtMaMi();
-
-       a.anMerBubStaGR();
-       a.anMerBubStaMiMa();
-       a.anMerBubStaMiMaRx();
-       a.anMerBubStaMaMi();
-
-       a.anMerBubEndGR();
-       a.anMerBubEndMiMa();
-       a.anMerBubEndMiMaRx();
-       a.anMerBubEndMaMi();
-
-       a.anMerQuickGR();
-       a.anMerQuickMiMa();
-       a.anMerQuickMiMaRx();
-       a.anMerQuickMaMi();
+//       a.anArrSrtGR();
+//       a.anArrSrtMiMa();
+//       a.anArrSrtMiMaRx();
+//       a.anArrSrtMaMi();
+//
+//       a.anBubStaGR();
+//       a.anBubStaMiMa();
+//       a.anBubStaMiMaRx();
+//       a.anBubStaMaMi();
+//
+//
+       a.anBubEndGR();
+//       a.anBubEndMiMa();
+//       a.anBubEndMiMaRx();
+//       a.anBubEndMaMi();
+//
+//       a.anQuickGR();
+//       a.anQuickMiMa();
+//       a.anQuickMiMaRx();
+//       a.anQuickMaMi();
+//
+//       a.anMerArrSrtGR();
+//       a.anMerArrSrtMiMa();
+//       a.anMerArrSrtMiMaRx();
+//       a.anMerArrSrtMaMi();
+//
+//       a.anMerBubStaGR();
+//       a.anMerBubStaMiMa();
+//       a.anMerBubStaMiMaRx();
+//       a.anMerBubStaMaMi();
+//
+//       a.anMerBubEndGR();
+//       a.anMerBubEndMiMa();
+//       a.anMerBubEndMiMaRx();
+//       a.anMerBubEndMaMi();
+//
+//       a.anMerQuickGR();
+//       a.anMerQuickMiMa();
+//       a.anMerQuickMiMaRx();
+//       a.anMerQuickMaMi();
 
     }
 }
--- Lab01/src/main/java/sorters/BubSrtEnd.java
+++ Lab01/src/main/java/sorters/BubSrtEnd.java
@@ -6,16 +6,25 @@ import java.util.Arrays;
 public class BubSrtEnd {
     public int[] srtBubEnd(int arr[]) {
         int temp;
-        for (int i = arr.length -1; i > 0; i--) {
-            for (int j = 0; j < arr.length -1; j++) {
-                System.out.print(Arrays.toString(arr) + " ");
-                System.out.println("j =" + j + " i =" + i);
+        int l =0;
+        int r = arr.length -1;
+        for (int i = r; i > l; i--) {
+            System.out.print(Arrays.toString(arr) + " ");
+            System.out.println(" i =" + i);
 
-                if (arr[j] > arr[j+1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+            if (arr[i - 1] > arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+        for (int j = l; j < r; j++) {
+            System.out.print(Arrays.toString(arr) + " ");
+            System.out.println("j =" + j );
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
         ArrPrinter.arrPr(arr);
